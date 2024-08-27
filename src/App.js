c19d61c013d3bdb5578d4440dad89063d503e749
Mila-Slavkovska <milaslavkovska@gmail.com>
2024-08-28 01:53:35 +0200

    Add two hand detection

--- src/App.js
+++ src/App.js
@@ -1,47 +1,112 @@
-import React, {useRef} from 'react';
-import * as tf from "@tensorflow/tfjs";
-import * as handpose from "@tensorflow-models/handpose";
-import Webcam from 'react-webcam';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import { FilesetResolver, HandLandmarker } from "@mediapipe/tasks-vision";
+import hand_landmarker_task from "./models/hand_landmarker.task";
+import { drawLandmarks } from "./utils/utilities";
+import "./App.css";
 
 function App() {
-  const webcamRef = useRef(null);
+  const videoRef = useRef(null);
   const canvasRef = useRef(null);
+  const [handPresence, setHandPresence] = useState(null);
+
+  useEffect(() => {
+    let handLandmarker;
+    let animationFrameId;
+
+    const initializeHandDetection = async () => {
+      try {
+        const vision = await FilesetResolver.forVisionTasks(
+          "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+        );
+        handLandmarker = await HandLandmarker.createFromOptions(vision, {
+          baseOptions: { modelAssetPath: hand_landmarker_task },
+          numHands: 2,
+          runningMode: "video",
+        });
+        detectHands();
+      } catch (error) {
+        console.error("Error initializing hand detection:", error);
+      }
+    };
+
+    const detectHands = () => {
+      if (videoRef.current && videoRef.current.readyState >= 2) {
+        const detections = handLandmarker.detectForVideo(
+          videoRef.current,
+          performance.now()
+        );
+        setHandPresence(detections.handednesses.length > 0);
+
+        if (detections.landmarks) {
+          drawLandmarks(detections.landmarks, canvasRef.current);
+        }
+      }
+      requestAnimationFrame(detectHands);
+    };
+
+    const startWebcam = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+        videoRef.current.srcObject = stream;
+        await initializeHandDetection();
+      } catch (error) {
+        console.error("Error accessing webcam:", error);
+      }
+    };
+
+    startWebcam();
+
+    return () => {
+      if (videoRef.current && videoRef.current.srcObject) {
+        videoRef.current.srcObject.getTracks().forEach((track) => track.stop());
+      }
+      if (handLandmarker) {
+        handLandmarker.close();
+      }
+      if (animationFrameId) {
+        cancelAnimationFrame(animationFrameId);
+      }
+    };
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Webcam 
-        ref={webcamRef}
-        style={{
-          position: "absolute",
-          marginLeft: "auto",
-          marginRight: "auto",
-          left: 0,
-          right: 0,
-          textAlign: "center",
-          zindex: 9,
-          width: 640,
-          height: 480
-        }}
-        />
-
-        <canvas 
-        ref={canvasRef}
-        style={{
-          position: "absolute",
-          marginLeft: "auto",
-          marginRight: "auto",
-          left: 0,
-          right: 0,
-          textAlign: "center",
-          zindex: 9,
-          width: 640,
-          height: 480
-        }}
-        />
-      </header>
-    </div>
+    <>
+      <h1>Is there a Hand? {handPresence ? "Yes" : "No"}</h1>
+      <div style={{ position: "relative" }}>
+        <video
+          ref={videoRef}
+          autoPlay
+          playsInline
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        ></video>
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        ></canvas>
+      </div>
+    </>
   );
 }
 
